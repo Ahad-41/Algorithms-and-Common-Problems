797536f2bf4626a4c7b51924b79d5b0a51e12f93
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2023-02-28 13:09:02 +0600

    Add files via upload

---
+++ Binary Exponentiation.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int M = 1e9+7;
+
+int binExpWithRecursion(int base, int power) {
+    if (!power) return 1; // base case
+    int result = binExpWithRecursion(base, power/2);
+    if (power & 1) return (result * (result * base) % M) % M;
+    return (result * result) % M;
+}
+
+int binExp(int base, int power) {
+    int result = 1;
+    while (power) {
+        if (power & 1) result = (result * 1LL * base) % M;
+        base = (base * 1LL * base) % M;
+        power /=  2;
+    }
+    return result;
+} 
+ 
+int main()
+{
+    int base, power; cin >> base >> power;
+    cout << binExp(base, power) << endl; 
+    cout << binExpWithRecursion(base, power) << endl;  
+} 
