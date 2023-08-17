ac34661449f1b94e73efb2ab139e44d369778efb
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2023-08-17 20:09:44 +0600

    Update Cycle Detection.cpp

--- Graph/Depth First Search/Cycle Detection.cpp
+++ Graph/Depth First Search/Cycle Detection.cpp
@@ -2,38 +2,56 @@
 using namespace std;
 typedef long long int ll;
 
-const ll N = 1e5 + 7;
+const ll N = 1e5+7;
+ll n, m, startNode, endNode, parent[N];
 vector<ll> adjList[N];
 bool visited[N];
 
-bool dfs(ll currNode, ll parent = -1) {
+bool dfs(ll currNode, ll par = -1) { 
     visited[currNode] = true;
 
-    bool isCycle = false;
     for (auto &u : adjList[currNode]) {
-        if (visited[u] and u == parent) continue;
-        if (visited[u]) return true;
-        isCycle |= dfs(u, currNode);
+        if(u == par) continue; 
+        if (visited[u]) {
+            endNode = currNode;
+            startNode = u;
+            return true;
+        }
+        parent[u] = currNode;
+        if (dfs(u, parent[u])) return true;
     }
-
-    return isCycle;
+    return false;
 }
 
-int main()
-{
-    ll node, edge; cin >> node >> edge;
-    while (edge--) {
+int main() {
+    cin >> n >> m;
+    while (m--) {
         ll u, v; cin >> u >> v;
         adjList[u].push_back(v);
         adjList[v].push_back(u);
     }
-
-    bool isCycle = false;
-    for (ll i = 1; i <= node; i++) {
-        if (visited[i]) continue;
-        isCycle = dfs(i);
-        if (isCycle) break;
+    
+    startNode = -1;
+    for (ll i = 1; i <= n; i++) {
+        if (!visited[i] and dfs(i)) break;
     }
 
-    cout << (isCycle ? "Cycle" : "Not Cycle") << endl;
+    if (startNode == -1) {
+        cout << "IMPOSSIBLE" << endl;
+        return 0;
+    }
+
+    vector<ll> cycle;
+    cycle.push_back(startNode);
+
+    ll currNode = endNode;
+    while (currNode != startNode) {
+        cycle.push_back(currNode);
+        currNode = parent[currNode];
+    } 
+    cycle.push_back(startNode);
+
+    cout << cycle.size() << endl;
+    for (ll v : cycle) cout << v << " ";
+    cout << endl;
 }
