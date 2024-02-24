87275a2cdeede025d5d5c1d1aeaa3083768c0048
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2024-02-24 17:44:15 +0600

    Update Longest Common Subsequence (LCS).cpp

--- Dynamic Programming/Longest Common Subsequence (LCS).cpp
+++ Dynamic Programming/Longest Common Subsequence (LCS).cpp
@@ -1,3 +1,4 @@
+// print the lcs length
 ll lcs(ll i, ll j, string &s1, string &s2) {
     if (i < 0 || j < 0) return 0;
     if (dp[i][j] != -1) return dp[i][j];
@@ -6,4 +7,35 @@ ll lcs(ll i, ll j, string &s1, string &s2) {
     ans = max(ans, lcs(i, j-1, s1, s2));
     ans = max(ans, lcs(i-1, j-1, s1, s2) + (s1[i] == s2[j]));
     return dp[i][j] = ans;
+}
+
+// print the lcs string
+string lcs(string a, string b) {
+    ll m = a.length(), n = b.length();
+    a = '@' + a;
+    b = '@' + b;
+    
+    ll dp[m+1][n+1];
+    memset(dp, 0, sizeof(dp));
+
+    for (ll i = 1; i <= m; i++) {
+        for (ll j = 1; j <= n; j++) {
+            if (a[i] == b[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+        }
+    }
+
+    ll i = m, j = n;
+    string ans;
+    while (i > 0 && j > 0) {
+        if (a[i] == b[j]) {
+            ans = a[i] + ans;
+            i--;
+            j--;
+        }
+        else if (dp[i-1][j] > dp[i][j-1]) i--;
+        else j--;
+    }
+
+    return ans;
 }
