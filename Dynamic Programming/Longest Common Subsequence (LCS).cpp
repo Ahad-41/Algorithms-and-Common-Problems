b6cd5b880efb61f3f21f026b96579acf6b397fc6
Md Ahad <109500162+Ahad-41@users.noreply.github.com>
2024-02-24 21:44:17 +0600

    Update Longest Common Subsequence (LCS).cpp

--- Dynamic Programming/Longest Common Subsequence (LCS).cpp
+++ Dynamic Programming/Longest Common Subsequence (LCS).cpp
@@ -8,32 +8,3 @@ ll lcs(ll i, ll j, string &s1, string &s2) {
     ans = max(ans, lcs(i-1, j-1, s1, s2) + (s1[i] == s2[j]));
     return dp[i][j] = ans;
 }
-
-// print the lcs string - iterative way
-string lcs(string a, string b) {
-    ll m = a.length(), n = b.length();
-    
-    ll dp[m+1][n+1];
-    memset(dp, 0, sizeof(dp));
-
-    for (ll i = 1; i <= m; i++) {
-        for (ll j = 1; j <= n; j++) {
-            if (a[i-1] == b[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
-            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-        }
-    }
-
-    ll i = m, j = n;
-    string ans;
-    while (i > 0 and j > 0) {
-        if (a[i-1] == b[j-1]) {
-            ans = a[i-1] + ans;
-            i--;
-            j--;
-        }
-        else if (dp[i-1][j] > dp[i][j-1]) i--;
-        else j--;
-    }
-
-    return ans;
-}
